b82f4b4a96edcf29bcd53bb8dd2a797f60444117
alhec <hector080896@gmail.com>
2020-08-10 19:12:37 -0400

    Assignment 1

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
-
 const app = express();
+
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(__dirname+'/public'));
-
-const dishRouter = require('./routes/dishRouter');
-
 app.use('/dishes', dishRouter);
-
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 
 app.use((req, res, next) => {
   res.statusCode = 200;
